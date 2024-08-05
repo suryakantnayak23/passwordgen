368ca56b83060a2fefc1343725d89a2d51df9c5c
Suryakant Nayak <suryakantnayak40@>
2024-08-05 15:27:55 +0530

    password generation and copying to the clipboard

---
+++ passwordgen/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import  PasswordUi from './components/PasswordUi';
+import PasswordGen from "./components/PasswordUi";
+function App() {
+  return (
+     < PasswordGen/>
+  );
+}
+
+export default App;
